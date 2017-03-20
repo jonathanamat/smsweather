621a40089d7d9cd8ad2555d904d31a5b51fdf2d4
jonathanamat <jonathanamat0218@gmail.com>
2017-03-20 19:55:17 -0400

    ...

--- send-weather.js
+++ send-weather.js
@@ -17,18 +17,7 @@ request({
   var client = require('twilio')(accountSid, authToken); 
  
   client.messages.create({ 
-    to: "+13525479441", 
-    from: "+13526190024", 
-    body: "Current Temp: " + weatherData.currently.temperature + "\xB0" + " \nOutside: " + weatherData.currently.summary + " \nRain Chance: " + weatherData.currently.precipProbability + "%",
-  }, function(err, message) { 
-    console.log(err);
-    console.log(message.sid); 
-  });
- 
-}); 
-
- client.messages.create({ 
-    to: "+13522740965", 
+    to: "+13525479441",
     from: "+13526190024", 
     body: "Current Temp: " + weatherData.currently.temperature + "\xB0" + " \nOutside: " + weatherData.currently.summary + " \nRain Chance: " + weatherData.currently.precipProbability + "%",
   }, function(err, message) { 
